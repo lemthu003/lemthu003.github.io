6d8b0ef8ce15ccb2d25fbec71038db16f83e434c
lemthu003 <73614272+lemthu003@users.noreply.github.com>
2025-10-16 16:33:10 -0700

    simplify

--- r.py
+++ r.py
@@ -9,122 +9,124 @@ def main():
     }
 
     # List of courses with grades and units
-    courses2 = [
+    courses_ug = [
         ("CHEM 31M", "B-", 5.00),
         ("CME 100", "A", 5.00),
         ("EMED 111A", "A", 5.00),
         ("PWR 1BRB", "A-", 4.00),
+
         ("BIO 84", "A", 4.00),
         ("COLLEGE 102", "A", 3.00),
         ("CS 103", "C-", 5.00),
         ("CS 106B", "A", 5.00),
         ("EMED 111B", "B+", 5.00),
+
         ("CHEM 33", "C", 5.00),
         ("COLLEGE 108", "A", 4.00),
         ("EMED 111C", "A-", 5.00),
         ("PATH 51", "A+", 4.00),
+
         ("CHEMENG 12SC", "A-", 2.00),
+
         ("CS 107", "B", 5.00),
         ("CS 109", "A-", 5.00),
         ("MATH 104", "C-", 4.00),
         ("PWR 2IY", "A-", 4.00),
+
         ("ARCHLGY 117", "A", 5.00),
         ("CS 161", "B", 5.00),
-        ("CS 224N", "A-", 4.00),
-        ("CS 152", "A", 3.00),
-        ("CS 181W", "B", 4.00),
-        ("CS 278", "B+", 4.00),
-        ("ENGR 76", "A", 5.00),
-        ("CS 147", "A", 5.00),
-        ("CS 147L", "A-", 3.00),
-        ("CS 148", "A+", 4.00),
-        ("CS 177", "A", 4.00),
-        ("VIETLANG 1", "A", 5.00),
-
-        ("VIETLANG 2", "A", 5.00),
-        ("CS 347", "A", 4.00),
-        ("CS 229", "A", 4.00),
-        ("CS 205L", "A+", 3.00),
-        ("CS 111", "A", 5.00),
-
-        ("VIETLANG 3", "A", 5.00),
-        ("CS 247G", "A", 4.00),
-        ("CS 194", "A", 3.00),
-        ("CS 231N", "A", 4.00),
-        ("HUM CLASS", "A", 3.00),
-        ("HUM CLASS", "A", 3.00),
-
-        ("Research", "A+", 3 * 22.00),
-    ]
-
-    courses = [
-        ("CHEM 31M", "B-", 5.00),
-        ("CME 100", "A", 5.00),
-        ("EMED 111A", "A", 5.00),
-        ("PWR 1BRB", "A-", 4.00),
-        ("BIO 84", "A", 4.00),
-        ("COLLEGE 102", "A", 3.00),
-        ("CS 103", "C-", 5.00),
-        ("CS 106B", "A", 5.00),
-        ("EMED 111B", "B+", 5.00),
-        ("CHEM 33", "C", 5.00),
-        ("COLLEGE 108", "A", 4.00),
-        ("EMED 111C", "A-", 5.00),
-        ("PATH 51", "A+", 4.00),
-        ("CHEMENG 12SC", "A-", 2.00),
-        ("CS 107", "B", 5.00),
-        ("CS 109", "A-", 5.00),
-        ("MATH 104", "C-", 4.00),
-        ("PWR 2IY", "A-", 4.00),
-        ("ARCHLGY 117", "A", 5.00),
-        ("CS 161", "B", 5.00),
-        ("CS 181W", "B", 4.00),
-        ("CS 278", "B+", 4.00),
-        ("ENGR 76", "A", 5.00),
-        ("CS 147", "A", 5.00),
-        ("CS 147L", "A-", 3.00),
         
+        ("CS 181W", "B", 4.00),
+        ("CS 278", "B+", 4.00),
+        ("ENGR 76", "A", 5.00),
+
+        ("CS 147", "A", 5.00),
+        ("CS 147L", "A-", 3.00),
         ("CS 177", "A", 4.00),
         ("VIETLANG 1", "A", 5.00),
 
-        ("VIETLANG 2", "A", 5.00),
-        ("CS 347", "A", 4.00),
+        ("VIETLANG 2", "A-", 5.00),
+        ("CS 347", "A-", 4.00),
+        ("CS 111", "B", 5.00),
 
-        ("CS 111", "A", 5.00),
-
-        ("VIETLANG 3", "A", 5.00),
+        ("VIETLANG 3", "A-", 5.00),
         ("CS 247G", "A", 4.00),
         ("CS 194", "A", 3.00),
-        ("HUM CLASS", "A", 3.00),
-        ("HUM CLASS", "A", 3.00),
+        
+        ("MUSIC 31N", "A-", 3.00),
+        ("URBANST 103C", "A-", 3.00),
 
     ]
-
-    courses3 = [
+    courses_grad = [
+        ("CS 224S", "A", 4.00),
+        ("CS 205L", "A+", 3.00),
+        ("CS 229", "B", 4.00),
+        ("CS 148", "A+", 4.00),
         ("CS 152", "A", 3.00),
         ("CS 224N", "A-", 4.00),
-        ("CS 148", "A+", 4.00),
-        ("CS 229", "A", 4.00),
-        ("CS 205L", "A+", 3.00),
-        ("CS 231N", "A", 4.00),
 
-        ("CS103", "C-", 5.00),
-        ("CS106B", "A", 5.00),
-        ("CS107", "B", 5.00),
-        ("CS109", "A-", 5.00),
-        ("CS161", "B", 5.00),
-        ("CS111", "A", 5.00),
+        ("CS 399", "A", 18.00),
 
-        ("Research", "A", 17.00),
     ]
 
-    # Calculate total grade points and total units attempted
-    total_grade_points = sum(grade_points[grade] * units for _, grade, units in courses)
-    total_units_attempted = sum(units for _, _, units in courses)
+    courses_all = [
+        ("CHEM 31M", "B-", 5.00),
+        ("CME 100", "A", 5.00),
+        ("EMED 111A", "A", 5.00),
+        ("PWR 1BRB", "A-", 4.00),
+        ("BIO 84", "A", 4.00),
+        ("COLLEGE 102", "A", 3.00),
+        ("CS 103", "C-", 5.00),
+        ("CS 106B", "A", 5.00),
+        ("EMED 111B", "B+", 5.00),
+        ("CHEM 33", "C", 5.00),
+        ("COLLEGE 108", "A", 4.00),
+        ("EMED 111C", "A-", 5.00),
+        ("PATH 51", "A+", 4.00),
+        ("CHEMENG 12SC", "A-", 2.00),
+        ("CS 107", "B", 5.00),
+        ("CS 109", "A-", 5.00),
+        ("MATH 104", "C-", 4.00),
+        ("PWR 2IY", "A-", 4.00),
+        ("ARCHLGY 117", "A", 5.00),
+        ("CS 161", "B", 5.00),
+        ("CS 224N", "A-", 4.00),
+        ("CS 152", "A", 3.00),
+        ("CS 181W", "B", 4.00),
+        ("CS 278", "B+", 4.00),
+        ("ENGR 76", "A", 5.00),
+        ("CS 147", "A", 5.00),
+        ("CS 147L", "A-", 3.00),
+        ("CS 148", "A+", 4.00),
+        ("CS 177", "A", 4.00),
+        ("VIETLANG 1", "A", 5.00),
 
-    # Compute GPA
-    gpa = total_grade_points / total_units_attempted
-    print(f"GPA: {gpa:.2f}")
+        ("VIETLANG 2", "A-", 5.00),
+        ("CS 347", "A-", 4.00),
+        ("CS 229", "B", 4.00),
+        ("CS 205L", "A+", 3.00),
+        ("CS 111", "B", 5.00),
+
+        ("VIETLANG 3", "A-", 5.00),
+        ("CS 247G", "A", 4.00),
+        ("CS 194", "A", 3.00),
+        ("CS 224S", "A", 4.00),
+        ("MUSIC 31N", "A-", 3.00),
+        ("URBANST 103C", "A-", 3.00)
+    ]
+
+
+    
+    total_courses = [courses_ug, courses_grad, courses_all]
+
+    for courses in total_courses:
+        # Calculate total grade points and total units attempted
+        total_grade_points = sum(grade_points[grade] * units for _, grade, units in courses)
+        total_units_attempted = sum(units for _, _, units in courses)
+
+        # Compute GPA
+        gpa = total_grade_points / total_units_attempted
+        print(f"GPA: {gpa:.2f}")
 
 if __name__ == "__main__":
     main()
